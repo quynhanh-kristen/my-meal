91d1e1cb12d6ee5f866354887e4bbbb08144ee5f
npqa <npqa@netcompany.com>
2023-07-26 14:39:53 +0700

    Revert "Add Meal card component"
    This reverts commit 4b29e42afd3f66f73dbd153b9ddd52a9f38346c0.

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,7 +3,7 @@ import MealCard from "../MealCard/MealCard";
 export default function Body() {
   return (
     <>
-      <MealCard cardName="Breakfast" />
+      <MealCard />
     </>
   );
 }
--- src/components/MealCard/MealCard.js
+++ src/components/MealCard/MealCard.js
@@ -1,29 +1,6 @@
-import { Card, CardActionArea, CardMedia, Typography } from "@mui/material";
+import { Card } from "@mui/material";
 import breakfast from "../../assets/img/d1.jpg";
-import "./styles.scss";
 
-export default function MealCard({ cardName = "Meal" }) {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea className="cardbox">
-        <CardMedia
-          component="img"
-          height="140"
-          image={breakfast}
-          alt={cardName}
-          className="card"
-        />
-        <div className="card-overlay">
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            className="text"
-          >
-            {cardName}
-          </Typography>
-        </div>
-      </CardActionArea>
-    </Card>
-  );
+export default function MealCard() {
+  return <Card src={breakfast} />;
 }
